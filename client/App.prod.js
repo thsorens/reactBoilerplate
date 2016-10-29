5b4a720ca54eca533d3e0a2a190ad047126cf897
Kristian Hellang <kristian@hellang.com>
2016-10-29 13:42:17 +0200

    OMG WTF LOLZ

--- client/App.prod.js
+++ client/App.prod.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
 import routes from 'routes';
 import 'styles/Site.scss';
 import store from 'reducers';
-import { syncParams } from 'routeParams';
+import {syncParams} from 'routeParams';
 
 syncParams(store, routes, browserHistory);
 
