c0ad1e6de6df8ab37d89a55cb5c3c05f572a0df9
Kristian Hellang <kristian@hellang.com>
2016-10-29 14:02:05 +0200

    Never mind...

--- client/App.dev.js
+++ client/App.dev.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import DevTools from './DevTools';
 import {Provider} from 'react-redux';
